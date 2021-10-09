8d754894a16c2c1b01c48373351d298fb1f99713
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-10-10 00:56:11 +0530

    Script update

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Undergraduate Student", "Full-Stack Developer", "Front-end Developer","Freelancer"],
+        strings: ["Full-Stack Developer", "Undergraduate Student", "Front-end Developer","Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Undergraduate Student", "Full-Stack Developer", "Front-end Developer","Freelancer"],
+        strings: ["Full-Stack Developer", "Undergraduate Student", "Front-end Developer","Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
